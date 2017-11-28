6c11d98472fdc85770233cf7910b964b773064b2
萨飞 <yongfu.xyf@alibaba-inc.com>
2017-11-28 21:05:28 +0800

    add

---
+++ productFlavors-more-finder/app/src/main/java/cn/test/productflavors/MainActivity.java
@@ -0,0 +1,36 @@
+package cn.test.productflavors;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import com.umeng.analytics.MobclickAgent;
+
+public class MainActivity extends Activity {
+    private String mPageName = MainActivity.class.getName();
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+        setContentView(R.layout.activity_main);
+        MobclickAgent.setSessionContinueMillis(1000);
+        MobclickAgent.setDebugMode(true);
+        MobclickAgent.openActivityDurationTrack(false);
+        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onPageStart(mPageName);
+        MobclickAgent.onResume(mContext);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPageEnd(mPageName);
+        MobclickAgent.onPause(mContext);
+    }
+}
